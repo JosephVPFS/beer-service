10fbc9f536e6017960a8173073ff8bfcf83af08f
Joseph VP <jvprakashfs@gmail.com>
2021-07-07 10:35:01 +0530

    Json Creator and JSON Property on BeerDTO

--- beer-service/src/main/java/com/jose/beers/web/model/BeerDto.java
+++ beer-service/src/main/java/com/jose/beers/web/model/BeerDto.java
@@ -12,6 +12,9 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -36,9 +39,11 @@ public class BeerDto {
     private Integer version;
 
 	@Null
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
 	
 	@Null
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
@@ -53,6 +58,7 @@ public class BeerDto {
 
     @NotNull
     @Positive
+    @JsonFormat(shape = Shape.STRING)
     private BigDecimal price;
 
     private Integer quantityOnHand;
